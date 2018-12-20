6725c9e64e50d2c06d28f53426cce7bfb6fae3f2
Isha Dijcks <ishadijcks@gmail.com>
2018-12-20 13:51:44 +0100

    Add comments top travis-build.sh

--- march_ws/travis-build.sh
+++ march_ws/travis-build.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+
+# The warning message to prepend at the trigger-dependent-build file.
 read -r -d '' warning_text << EndOfMessage
 #
 # IMPORTANT NOTE!
@@ -8,19 +10,22 @@ read -r -d '' warning_text << EndOfMessage
 #
 EndOfMessage
 
-
+# Preserve the original shebang.
 shebang=$(head -n 2 ./trigger-dependent-build-base.sh)
 
+# Remove the comments from the base file and store the results in the trigger-dependent-build.sh.
 grep -o '^[^#]*' trigger-dependent-build-base.sh > trigger-dependent-build.sh
 
+# Prepend the warning message and shebang.
 echo "$warning_text" | cat - trigger-dependent-build.sh > temp && mv temp trigger-dependent-build.sh
 echo "$shebang" | cat - trigger-dependent-build.sh > temp && mv temp trigger-dependent-build.sh
 
-
+# Move the travis.yml and created file to each submodule.
 git submodule foreach cp ../../trigger-dependent-build.sh .
 git submodule foreach cp ../../../.travis-submodule-base.yml .
 git submodule foreach mv .travis-submodule-base.yml .travis.yml
 
+# Remove the temporary created file.
 rm ./trigger-dependent-build.sh
 
 
