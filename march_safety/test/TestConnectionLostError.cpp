25904963272e0f67cc0cd4dd18a8b0a028eca994
Isha Dijcks <ishadijcks@gmail.com>
2019-07-23 17:09:30 +0200

    Update march_safety/test/TestConnectionLostError.cpp

--- march_safety/test/TestConnectionLostError.cpp
+++ march_safety/test/TestConnectionLostError.cpp
@@ -87,7 +87,7 @@ TEST_F(TestConnectionLostError, connectionLost)
   timeMessage.data = ros::Time::now();
   pub_alive.publish(timeMessage);
   ros::spinOnce();
-  int sleep_ms = send_errors_interval * 0.9 + input_device_connection_timeout;
+  int sleep_ms = send_errors_interval / 2.0 + input_device_connection_timeout;
   ros::Duration(sleep_ms / 1000.0).sleep();
   ros::spinOnce();
 
