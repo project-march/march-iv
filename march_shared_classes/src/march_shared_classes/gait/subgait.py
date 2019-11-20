e8d759a19666a06ec53dfd88bd23cc3f43a74064
roel <roel@vossenopreis.nl>
2019-11-20 13:43:12 +0100

    Migrate import of gait files from gait generator to Subgait parent class.

--- march_shared_classes/src/march_shared_classes/gait/subgait.py
+++ march_shared_classes/src/march_shared_classes/gait/subgait.py
@@ -2,9 +2,15 @@ import rospy
 from trajectory_msgs.msg import JointTrajectory
 from trajectory_msgs.msg import JointTrajectoryPoint
 from march_shared_resources.msg import Setpoint
+from limits import Limits
+from joint_trajectory import JointTrajectory
+import yaml
+from rospy_message_converter import message_converter
 
 
 class Subgait:
+    joint_class = JointTrajectory
+
     def __init__(self, joints, duration, gait_type='walk_like',
                  name='Walk', subgait='right_open', version='First try', description='Just a simple gait'):
         # Set gait_type to walk_like if an old file with no gait_type is opened
@@ -18,6 +24,53 @@ class Subgait:
         self.version = version
         self.description = str(description)
         self.duration = duration
+
+    @classmethod
+    def from_file(cls, robot, file_name):
+        if file_name is None or file_name == "":
+            return None
+        try:
+            gait_name = file_name.split("/")[-3]
+            subgait_name = file_name.split("/")[-2]
+            version = file_name.split("/")[-1].replace(".subgait", "")
+            march_subgait_yaml = yaml.load(open(file_name), Loader=yaml.SafeLoader)
+            march_subgait = message_converter.convert_dictionary_to_ros_message(
+                'march_shared_resources/Subgait', march_subgait_yaml)
+        except Exception as e:
+            rospy.logerr(str(e))
+            return None
+        return cls.from_msg(robot, march_subgait, gait_name, subgait_name, version)
+
+    @classmethod
+    def from_msg(cls, robot, subgait_msg, gait_name, subgait_name, version):
+        if robot is None:
+            rospy.logerr("Cannot create gait without a loaded robot.")
+            return None
+
+        joint_trajectory = subgait_msg.trajectory
+        duration = rospy.Duration(subgait_msg.duration.secs, subgait_msg.duration.nsecs).to_sec()
+        joint_list = []
+        for joint_name in joint_trajectory.joint_names:
+            urdf_joint = cls._get_joint_from_urdf(robot, joint_name)
+            if urdf_joint is None:
+                rospy.logwarn("Not all joints in gait are in robot.")
+                continue
+
+            limits = Limits(urdf_joint.safety_controller.soft_lower_limit,
+                            urdf_joint.safety_controller.soft_upper_limit,
+                            urdf_joint.limit.velocity)
+
+            joint_list.append(cls.joint_class.from_msg(subgait_msg, joint_name, limits, duration))
+
+        return cls(joint_list, duration, subgait_msg.gait_type, gait_name, subgait_name,
+                   version, subgait_msg.description)
+
+    @staticmethod
+    def _get_joint_from_urdf(robot, joint_name):
+        for urdf_joint in robot.joints:
+            if urdf_joint.name == joint_name:
+                return urdf_joint
+        return None
 
     def to_joint_trajectory(self):
         joint_trajectory = JointTrajectory()
