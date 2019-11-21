829b4c54f25f8f74b1b69927c7a8a913b4674d62
Project March <tech@projectmarch.nl>
2019-11-21 11:06:37 +0100

    Wait for gait server instead of controller

---
+++ march_state_machine/src/march_state_machine/states/wait_for_gait_server_state.py
@@ -0,0 +1,22 @@
+import actionlib
+import rospy
+import smach
+
+from march_shared_resources.msg import GaitNameAction
+
+
+class WaitForGaitServerState(smach.State):
+    """State which waits for the ros controller manager to load the controllers."""
+
+    def __init__(self, timeout=rospy.Duration.from_sec(60)):
+        self._timeout = timeout
+        smach.State.__init__(self, outcomes=['succeeded', 'failed'])
+
+    def execute(self, _userdata):
+        rospy.logdebug('Waiting for /march/gait/perform action server')
+        gait_client = actionlib.SimpleActionClient('/march/gait/perform', GaitNameAction)
+
+        if gait_client.wait_for_server(self._timeout):
+            return 'succeeded'
+        else:
+            return 'failed'
