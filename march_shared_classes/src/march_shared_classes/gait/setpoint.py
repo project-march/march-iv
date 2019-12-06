ac02ac9aaaf2fe3e6af61ea31e2f0165cd0a3447
jorisweeda <joris.weeda@hotmail.com>
2019-12-06 18:30:05 +0100

    moved digits back to the class.
    This was necessary because the gait generator was using the same class and the digits attribute.

--- march_shared_classes/src/march_shared_classes/gait/setpoint.py
+++ march_shared_classes/src/march_shared_classes/gait/setpoint.py
@@ -1,14 +1,12 @@
 
-
-AMOUNT_OF_DECIMALS = 4
-
-
 class Setpoint(object):
     """Base class to define the setpoints of a subgait"""
+    digits = 4
+
     def __init__(self, time, position, velocity):
-        self.time = round(time, AMOUNT_OF_DECIMALS)
-        self.position = round(position, AMOUNT_OF_DECIMALS)
-        self.velocity = round(velocity, AMOUNT_OF_DECIMALS)
+        self.time = round(time, self.digits)
+        self.position = round(position, self.digits)
+        self.velocity = round(velocity, self.digits)
 
     def __repr__(self):
         return 'Time: %s, Position: %s, Velocity: %s' % (self.time, self.position, self.velocity)
