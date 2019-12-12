78935f7dbe22f9c1d40fca7a3d19957351fce5d3
Olav de Haas <o.n.dehaas@student.tudelft.nl>
2019-12-12 11:02:49 +0100

    Add documentation on custom state machines

--- march_state_machine/src/march_state_machine/state_machines/step_state_machine.py
+++ march_state_machine/src/march_state_machine/state_machines/step_state_machine.py
@@ -5,8 +5,19 @@ from march_state_machine.states.gait_state import GaitState
 
 
 class StepStateMachine(smach.Sequence):
+    """A smach.StateMachine that implements a gait with a sequence of subgaits."""
 
     def __init__(self, gait_name, subgaits=None):
+        """Initializes the state machine with a gait name and a sequence of subgaits.
+
+        The subgaits will be performed in the sequence given.
+
+        :type gait_name: str
+        :param gait_name: Name of the gait to perform in sequence
+        :type subgaits: list(str)
+        :param subgaits: List of subgaits to be performed in the given order.
+                         When None, the subgaits will be ['right_open', 'left_close'].
+        """
         super(StepStateMachine, self).__init__(outcomes=['succeeded', 'preempted', 'failed'],
                                                connector_outcome='succeeded')
 
