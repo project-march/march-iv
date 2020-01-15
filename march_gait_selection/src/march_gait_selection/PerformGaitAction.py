0147fbfd1db9eef3f44b984b9f2f84291a9c41d5
jorisweeda <joris.weeda@hotmail.com>
2020-01-15 15:54:38 +0100

    Migrated the transition functions to a newly created  class which calculates.
    This class creates a new subgait object which has a transition between a given last executed gait and the new scheduled gait. The requirement would be that the from and to subgait order is identical.

--- march_gait_selection/src/march_gait_selection/PerformGaitAction.py
+++ march_gait_selection/src/march_gait_selection/PerformGaitAction.py
@@ -2,7 +2,6 @@ import actionlib
 import rospy
 
 from march_shared_classes.exceptions.general_exceptions import MsgTypeError
-from march_shared_classes.exceptions.gait_exceptions import *
 from march_shared_resources import msg
 from march_shared_resources.msg import GaitAction, GaitGoal, GaitNameAction
 
@@ -44,44 +43,6 @@ class PerformGaitAction(object):
         self.action_server.set_aborted('Gait {gn} does not exist in parsed gaits'
                                        .format(gn=subgait_goal_msg.name))
         return False
-
-    def add_transition_trajectory(self, current_gait, new_gait, last_subgait):
-        current_gait_object = self.gait_selection[current_gait]
-        new_gait_object = self.gait_selection[new_gait]
-
-        if current_gait_object is None:
-            raise GaitError(msg='{gn} not found in parsed gait names from gait selection'
-                            .format(gn=current_gait))
-
-        if new_gait_object is None:
-            raise GaitError(msg='{gn} not found in parsed gait names from gait selection'
-                            .format(gn=new_gait))
-
-        if current_gait_object.to_subgaits_names != new_gait_object.to_subgaits_names:
-            raise GaitError(msg='To_subgait list do not match between gait: {cg} and gait: {ng}'
-                            .format(cg=current_gait, ng=new_gait))
-
-        if current_gait_object.from_subgaits_names != new_gait_object.from_subgaits_names:
-            raise GaitError(msg='From_subgait list do not match between gait: {cg} and gait: {ng}'
-                            .format(cg=current_gait, ng=new_gait))
-
-        new_subgait_index = current_gait_object.to_subgaits_names.index(last_subgait)
-
-        old_subgait_object = current_gait_object[last_subgait]
-        new_subgait_object = new_gait_object.to_subgaits_names[new_subgait_index]
-
-        if old_subgait_object is None:
-            raise SubgaitNameNotFound(msg='Subgait {sg} not found in to_subgaits of gait {gn}'
-                                      .format(sg=last_subgait, gn=current_gait))
-
-        if new_subgait_object is None:
-            raise SubgaitNameNotFound(msg='Subgait {sg} not found in from_subgaits of gait {gn}'
-                                      .format(sg=last_subgait, gn=current_gait))
-
-        for joint_new_gait in new_subgait_object.joints:
-            old_subgait_joint = old_subgait_object[joint_new_gait.name]
-            joint_last_setpoint = old_subgait_joint.setpoints[-1]
-            old_subgait_joint.setpoints.insert(0, joint_last_setpoint)
 
     def schedule_gait(self, gait_name, subgait):
         """Construct the goal message and send."""
---
+++ march_gait_selection/src/march_gait_selection/create_transition.py
@@ -0,0 +1,107 @@
+from march_shared_classes.exceptions.gait_exceptions import *
+from march_shared_classes.gait.subgait import Subgait
+
+
+class TransitionSubgait:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def create_transition_subgait(gait_selection, current_gait_name, new_gait_name, last_subgait_name,
+                                  transition_time=500000000):
+        """ """
+        current_gait_object = gait_selection[current_gait_name]
+        new_gait_object = gait_selection[new_gait_name]
+
+        if current_gait_object is None:
+            raise GaitError(msg='{gn} not found in parsed gait names from gait selection'
+                            .format(gn=current_gait_name))
+
+        if new_gait_object is None:
+            raise GaitError(msg='{gn} not found in parsed gait names from gait selection'
+                            .format(gn=new_gait_name))
+
+        if current_gait_object.to_subgaits_names != new_gait_object.to_subgaits_names:
+            raise GaitError(msg='To_subgait list do not match between gait: {cg} and gait: {ng}'
+                            .format(cg=current_gait_name, ng=new_gait_name))
+
+        if current_gait_object.from_subgaits_names != new_gait_object.from_subgaits_names:
+            raise GaitError(msg='From_subgait list do not match between gait: {cg} and gait: {ng}'
+                            .format(cg=current_gait_name, ng=new_gait_name))
+
+        old_subgait, new_subgait = TransitionSubgait.get_start_end_subgaits(current_gait_object,
+                                                                            new_gait_object,
+                                                                            last_subgait_name)
+
+        robot = gait_selection.robot
+        gait_name = 'transition'
+        subgait_name = 'transition_subgait'
+        version = 'default'
+
+        duration = TransitionSubgait.transition_duration(transition_time)
+        trajectory = TransitionSubgait.transition_trajectory(old_subgait, new_subgait, transition_time)
+
+        subgait_dict = {'trajectory': trajectory,
+                        'duration': duration}
+
+        return Subgait.from_dict(robot, subgait_dict, gait_name, subgait_name, version)
+
+    @staticmethod
+    def transition_duration(transition_time):
+        """ """
+        return {'secs': 0, 'nsecs': transition_time}
+
+    @staticmethod
+    def transition_trajectory(old_subgait, new_subgait, interval_time):
+        """ """
+        trajectory = {}
+
+        joints = []
+        positions = [[], []]
+        velocities = [[], []]
+        for joint_old_gait in old_subgait.joints:
+            joint_new_gait = new_subgait.get_joint(joint_old_gait.name)
+
+            old_setpoint = joint_old_gait.setpoints[-1]
+            new_setpoint = joint_new_gait.setpoints[0]
+
+            positions[0].append(old_setpoint.position)
+            positions[1].append(new_setpoint.position)
+
+            velocities[0].append(old_setpoint.velocity)
+            velocities[1].append(new_setpoint.velocity)
+
+            joints.append(joint_old_gait.name)
+
+        points = []
+        transition_time = 0
+        for position_list, velocity_list in zip(positions, velocities):
+            point = {'positions': position_list,
+                     'velocities': velocity_list,
+                     'time_from_start': {'secs': 0,
+                                         'nsecs': transition_time}
+                     }
+            transition_time += interval_time
+            points.append(point)
+
+        trajectory['joint_names'] = joints
+        trajectory['points'] = points
+
+        return trajectory
+
+    @staticmethod
+    def get_start_end_subgaits(old_gait, new_gait, last_subgait_name):
+        """ """
+        new_subgait_index = new_gait.from_subgaits_names.index(last_subgait_name)
+        new_subgait_name = new_gait.to_subgaits_names[new_subgait_index]
+
+        old_subgait = old_gait[last_subgait_name]
+        new_subgait = new_gait[new_subgait_name]
+
+        if old_subgait is None:
+            raise SubgaitNameNotFound(subgait_name=last_subgait_name)
+
+        if new_subgait is None:
+            raise SubgaitNameNotFound(subgait_name=new_subgait_name)
+
+        return old_subgait, new_subgait
