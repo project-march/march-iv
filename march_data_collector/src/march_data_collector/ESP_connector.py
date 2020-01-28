3eead96e8776564b16b1d4fc1b5e4886b8553fb2
RutgerVanBeek <rvbG14-22>
2020-01-28 14:20:25 +0100

    ESP pubsubApi joint message

--- march_data_collector/src/march_data_collector/ESP_connector.py
+++ march_data_collector/src/march_data_collector/ESP_connector.py
@@ -2,21 +2,36 @@
 import socket
 import esppy
 import datetime
+import atexit
 
 
-def receive_messages(input_ip, port, pub, window):
+def receive_messages(input_ip, port, pub_joint_angles, window_joint_angles, esp):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind((input_ip, port))
+
+    # compute_window =esp.get_window("March_test/March_cq/computeWindow")
+    # copy_window = esp.get_window("March_test/March_cq/copyWindow")
     i = 0
-    while i<100:
+    while True:
+        i = i+1
         data, addr = sock.recvfrom(1024)
         datachannels = data.split()
-        values = [float(val) for val in datachannels[1:]]
-        timestr = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        csv = '1,' + timestr + ',"' + datachannels[0].decode("UTF-8")+'",' + ",".join([str(value) for value in values]) + "\n"
-        pub.send(csv)
-        print(window.get_events())
-        i = i +1
+        timestr = datetime.datetime.fromtimestamp(float(datachannels[1])).strftime("%Y-%m-%d %H:%M:%S.%f")
+        type = datachannels[0].decode("UTF-8")
+        if type=="joint_angles":
+            values = [float(val) for val in datachannels[2:]]
+            csv = '1,' + timestr + ',[' + ";".join([str(value) for value in values]) + "]\n"
+            pub_joint_angles.send(csv)
+            print("added joint_angle")
+        # if type=="temperature":
+        #     value = float(datachannels[2])
+        #     csv = '1,' + timestr + ',' + str(value)+'\n'
+        #     pub_temperature.send(csv)
+
+
+def exit_function(pub1):
+    print('Goodbye')
+    pub1.close()
 
 
 
@@ -24,22 +39,10 @@ if __name__ == '__main__':
     url = "http://localhost:9900"
     esp = esppy.ESP(url)
     print(esp)
-    window = esp.get_window("March_test/March_cq/March")
-    print(window)
+    window_joint_angles  = esp.get_window("test_project/contquery/sourceWindowJoint")
+    # window_temperature  = esp.get_window("March_test/March_cq/sourceWindowTemperature")
 
-
-    #
-    pub = window.create_publisher(blocksize=1, dateformat="%Y-%m-%d %H:%M:%S")
-    # pub.send('1,"a", 1\n')
-    # print(window.get_events())
-    # pub.send(str(time.time())+",a,2,234,234")
-    # pub.send(str(time.time())+",a,3")
-    # sub = esppy.windows.Subscriber(window, format="csv", on_event=on_event)
-    # sub.start()
-
-
-
-    receive_messages("127.0.0.1", 32000, pub, window)
-    #/publishers/March_test/March_cq/March/?blocksize=1&dateformat=%Y%m%dT%H:%M:%S.%f&format=csv&opcode=insert&pause=0&rate=0
-
-    pub.close()
+    pub_joint_angles = window_joint_angles.create_publisher(blocksize=1, dateformat="%Y-%m-%d %H:%M:%S")
+    # pub_temperature = window_temperature.create_publisher(blocksize=1, dateformat="%Y-%m-%d %H:%M:%S")
+    atexit.register(exit_function, pub_joint_angles)
+    receive_messages("127.0.0.1", 32000, pub_joint_angles, window_joint_angles, esp)
--- march_data_collector/src/march_data_collector/data_collector_node.py
+++ march_data_collector/src/march_data_collector/data_collector_node.py
@@ -24,9 +24,11 @@ dfesphome = os.environ["DFESP_HOME"]
 sys.path.append(dfesphome + '/lib')
 import pubsubApi
 import modelingApi
-import pubsubInternal
+import datetime
 
 import lxml.etree as etree
+
+
 
 import socket
 
@@ -35,15 +37,16 @@ import socket
 
 class DataCollectorNode(object):
 
-    def __init__(self, com_calculator, cp_calculators):
-        self._com_calculator = com_calculator
-        self._cp_calculators = cp_calculators
+    # def __init__(self,  com_calculator, cp_calculators):
+    def __init__(self):
+        # joint_names = rospy.get_param('/march/joint_names')
+        joint_names = ['left_hip_aa', 'left_hip_fe', 'left_knee', 'left_ankle', 'right_hip_aa',
+                       'right_hip_fe', 'right_knee', 'right_ankle']
 
-        joint_names = rospy.get_param('/march/joint_names')
 
-        self._imu_broadcaster = tf2_ros.TransformBroadcaster()
-        self._com_marker_publisher = rospy.Publisher('/march/com_marker', Marker, queue_size=1)
-
+        # self._imu_broadcaster = tf2_ros.TransformBroadcaster()
+        # self._com_marker_publisher = rospy.Publisher('/march/com_marker', Marker, queue_size=1)
+        #
         self._temperature_subscriber = [rospy.Subscriber('/march/temperature/' + joint,
                                                          Temperature,
                                                          self.temperature_callback, joint) for joint in joint_names]
@@ -52,41 +55,143 @@ class DataCollectorNode(object):
                                                              JointTrajectoryControllerState,
                                                              self.trajectory_state_callback)
 
-        self._imc_state_subscriber = rospy.Subscriber('/march/imc_states', ImcErrorState, self.imc_state_callback)
-
+        # self._imc_state_subscriber = rospy.Subscriber('/march/imc_states', ImcErrorState, self.imc_state_callback)
+        #
         self._imu_subscriber = rospy.Subscriber('/march/imu', Imu, self.imu_callback)
+        #below should match with the source windows in the xml file
+        sourceWindows = ["jointSourceWindow", ["TemperatureWindow_" + joint for joint in joint_names], "IMUsource"]
 
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.host = "localhost"
-        self.port = 32000
+        def pubErrCbFunc(failure, code, ctx):
+            # don't output anything for client busy
+            if failure == pubsubApi.pubsubFail_APIFAIL and code == pubsubApi.pubsubCode_CLIENTEVENTSQUEUED:
+                return
+
+            failMsg = pubsubApi.DecodeFailure(failure)
+            codeMsg = pubsubApi.DecodeFailureCode(code)
+            print('Client services error: ' + failMsg + codeMsg)
+
+        ret = pubsubApi.Init(modelingApi.ll_Off, None)
+        if ret == 0:
+            rospy.logwarn(" Could not initialize pubsub library")
+        logger = logging.getLogger()
+        logger.addHandler(modelingApi.getLoggingHandler())
+
+        pubErrFunc = pubsubApi.ERRCBFUNC(pubErrCbFunc)
+
+        basic_url = 'dfESP://localhost:9901'
+        project = basic_url + '/March_test'
+        contquery = project + '/March_cq'
+        jointwindow = contquery + '/sourceWindowJoint'
+
+        # create the following dictionaries schemaptr, publisher
+
+        self.esp_publishers = {}
+        for source in sourceWindows:
+            window_url = contquery + "/" + source +"?get=schema"
+            stringv = pusubApi.QueryMeta(window_url)
+            if stringv == None:
+                rospy.logwarn('Could not get ESP source window schema for window ' +source)
+                continue
+                # pubsubApi.Shutdown()
+
+            schema = modelingApi.StringVGet(stringv, 0)
+
+            if schema == None:
+                rospy.logwarn('Could not get ESP schema from query response for source ' + source)
+                continue
+            schemaptr = modelingApi.SchemaCreate(source, self.schema)
+
+            if schemaptr == None:
+                rospy.logwarn('Could not build ESP source window schema for source ' + source)
+                continue
+
+            pub = pubsubApi.PublisherStart(jointwindow, pubErrFunc, None)
+            if pub == None:
+                rospy.logwarn('Could not create ESP publisher client for source' + source)
+                continue
+
+            ret = pubsubApi.Connect(pub)
+            if ret != 1:
+                logger.error('Could not connect ESP publisher client for source ' + source)
+                pubsubApi.Stop(pub, 0)
+                continue
+                pubsubApi.Shutdown()
+
+            self.esp_publishers[source] = (pub, schemaptr)
 
 
+
+
+
+
+
+
+
+
+
+
+        modelingApi.StringVFree(stringv)
+
+
+
+
+
+
+        values = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8]
+        csv = 'i,n,1'
+        # timestr = datetime.datetime.fromtimestamp(float(datachannels[1])).strftime("%Y-%m-%d %H:%M:%S")
+        timestr = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
+        # print(timestr)
+
+
+        # csv = 'i, n, 1,' +timestr
+
+
+        # pubsubApi.Stop(pub, 1)
+        # pubsubApi.Shutdown()
 
 
     def send_udp(self, msg):
         self.sock.sendto(msg.encode('utf-8'), (self.host, self.port))
 
+    def send_to_esp(self, csv, source):
+        rospy.loginfo(csv)
+        pub, schemaptr = self.esp_publishers[source]
+        event = modelingApi.EventCreate2(schemaptr, csv, "%Y-%m-%d %H:%M:%S")
+        eventVector = modelingApi.EventVCreate()
+        modelingApi.EventVPushback(eventVector, event)
+        eventBlock = modelingApi.EventBlockNew1(eventVector, modelingApi.ebt_NORMAL)
 
-    def temperature_callback(self, data, joint):
-        rospy.logdebug('Temperature' + joint + ' is ' + str(data.temperature))
+        ret = pubsubApi.PublisherInject(pub, eventBlock)
+        print ("Return value inject = " +str(ret))
+        modelingApi.EventBlockDestroy(eventBlock)
+
+
+    # def temperature_callback(self, data, joint):
+    #     # rospy.logwarn("Temperature")
+    #     msg = " ".join(["temperature",str(data.header.stamp.secs + data.header.stamp.nsecs * 10 **(-9)), str(data.temperature)])
+    #     self.send_udp(msg)
+
 
     def trajectory_state_callback(self, data):
-        rospy.logdebug('received trajectory state' + str(data.desired))
+        rospy.logwarn('received trajectory state' + str(data.desired))
         joint_angles = data.actual.positions
-        angle_str = " ".join([str(angle) for angle in joint_angles])
-        msg = " ".join(["joinst_angles", angle_str])
-        self.send_udp(msg)
+        time = data.header.stamp.secs + data.header.stamp.nsecs * 10**(-9)
+        timestr = datetime.datetime.fromtimestamp(time).strftime("%Y-%m-%d %H:%M:%S.%f")
+        csv = 'i, n, 1,' + timestr + ',[' + ";".join([str(value) for value in joint_angles]) + "]"
+        self.send_to_esp(csv)
+        #
+        # com = self._com_calculator.calculate_com()
+        # self._com_marker_publisher.publish(com)
+        # for cp_calculator in self._cp_calculators:
+        #     cp_calculator.calculate_cp(com)
 
-        com = self._com_calculator.calculate_com()
-        self._com_marker_publisher.publish(com)
-        for cp_calculator in self._cp_calculators:
-            cp_calculator.calculate_cp(com)
-
-    def imc_state_callback(self, data):
-        rospy.logdebug('received IMC message current is ' + str(data.current))
-
+    # def imc_state_callback(self, data):
+    #     rospy.logdebug('received IMC message current is ' + str(data.current))
+    #
     def imu_callback(self, data):
         if data.header.frame_id == 'imu_link':
+            rospy.loginfo(data)
             transform = TransformStamped()
 
             transform.header.stamp = rospy.Time.now()
@@ -110,13 +215,13 @@ class DataCollectorNode(object):
 def main():
     rospy.init_node('data_collector', anonymous=True)
 
-    robot = URDF.from_parameter_server()
-    tf_buffer = tf2_ros.Buffer()
-    tf2_ros.TransformListener(tf_buffer)
-    center_of_mass_calculator = CoMCalculator(robot, tf_buffer)
-    feet = ['ankle_plate_left', 'ankle_plate_right']
-    cp_calculators = [CPCalculator(tf_buffer, foot) for foot in feet]
+    # robot = URDF.from_parameter_server()
+    # tf_buffer = tf2_ros.Buffer()
+    # tf2_ros.TransformListener(tf_buffer)
+    # center_of_mass_calculator = CoMCalculator(robot, tf_buffer)
+    # feet = ['ankle_plate_left', 'ankle_plate_right']
+    # cp_calculators = [CPCalculator(tf_buffer, foot) for foot in feet]
 
-    DataCollectorNode(center_of_mass_calculator, cp_calculators)
-
+    # DataCollectorNode(center_of_mass_calculator, cp_calculators)
+    DataCollectorNode()
     rospy.spin()
