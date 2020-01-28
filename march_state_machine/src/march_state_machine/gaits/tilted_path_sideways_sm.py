6d73eeb732bed8ede7f3b787e122760cff64700a
marnixbrands <marnix_456@live.nl>
2020-01-28 11:55:46 +0100

    Small fix of tilted path sideways state machine

--- march_state_machine/src/march_state_machine/gaits/tilted_path_sideways_sm.py
+++ march_state_machine/src/march_state_machine/gaits/tilted_path_sideways_sm.py
@@ -7,15 +7,15 @@ from march_state_machine.states.idle_state import IdleState
 def create():
     sm_tilted_path_sideways = smach.StateMachine(outcomes=['succeeded', 'preempted', 'failed'])
     with sm_tilted_path_sideways:
-        smach.StateMachine.add('GAIT TP STRAIGHT FIRST', StepStateMachine('tilted_path_first_start',
-                                                                          subgaits=['left_open', 'right_close']),
+        smach.StateMachine.add('GAIT TP FIRST START', StepStateMachine('tilted_path_first_start',
+                                                                       subgaits=['left_open', 'right_close']),
                                transitions={'succeeded': 'STANDING TP SIDEWAYS', 'failed': 'failed'})
 
         smach.StateMachine.add('STANDING TP SIDEWAYS', IdleState(outcomes=['gait_tilted_path_second_start',
                                                                            'preempted']),
-                               transitions={'gait_tilted_path_second_start': 'GAIT TP STRAIGHT SECOND'})
+                               transitions={'gait_tilted_path_second_start': 'GAIT TP SECOND START'})
 
-        smach.StateMachine.add('GAIT TP STRAIGHT SECOND', StepStateMachine('tilted_path_second_start',
-                                                                           subgaits=['left_open', 'right_close']))
+        smach.StateMachine.add('GAIT TP SECOND START', StepStateMachine('tilted_path_second_start',
+                                                                        subgaits=['left_open', 'right_close']))
 
     return sm_tilted_path_sideways
