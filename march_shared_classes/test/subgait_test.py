1eb5a641dddc86a13f06927b5f6c59ecbc13059e
roel <roel@vossenopreis.nl>
2020-02-03 19:02:31 +0100

    Write some tests

--- march_shared_classes/test/subgait_test.py
+++ march_shared_classes/test/subgait_test.py
@@ -9,14 +9,20 @@ import xacro
 
 class SubgaitTest(unittest.TestCase):
     def setUp(self):
-        resources_folder = rospkg.RosPack().get_path("march_shared_classes") + "/test/resources"
-        urdf_path = resources_folder + "/march4.urdf"
-        robot = urdf.Robot.from_xml_file(urdf_path)
-        subgait_path = resources_folder + "/walk/left_swing/MV_walk_leftswing_v2.subgait"
-        self.subgait = Subgait.from_file(robot, subgait_path)
+        self.resources_folder = rospkg.RosPack().get_path("march_shared_classes") + "/test/resources"
+        self.robot = urdf.Robot.from_xml_file(self.resources_folder + "/march4.urdf")
+        self.subgait_path = self.resources_folder + "/walk/left_swing/MV_walk_leftswing_v2.subgait"
+        self.subgait = Subgait.from_file(self.robot, self.subgait_path)
 
+    # Subgait.from_file tests
     def test_from_file_valid_path(self):
-        self.assertIsNotNone(self.subgait)
+        self.assertIsInstance(self.subgait, Subgait)
 
-    def test_something_2(self):
-        self.assertEqual(True, True)
+    def test_from_file_invalid_path(self):
+        self.assertIsNone(Subgait.from_file(self.robot, self.resources_folder + "/MV_walk_leftswing_v2.subgait"))
+
+    def test_from_file_none_path(self):
+        self.assertIsNone(Subgait.from_file(self.robot, None))
+
+    def test_from_file_no_robot(self):
+        self.assertIsNone(Subgait.from_file(None, self.subgait_path))
