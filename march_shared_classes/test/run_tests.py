092916b9abf0d1aeefa3ea1bf83afd53f5ee57b5
roel <roel@vossenopreis.nl>
2020-02-03 17:24:55 +0100

    Import Subgait from file test

--- march_shared_classes/test/run_tests.py
+++ march_shared_classes/test/run_tests.py
@@ -4,6 +4,7 @@ import rosunit
 from .joint_trajectory_test import JointTrajectoryTest
 from .limits_test import LimitsTest
 from .setpoint_test import SetpointTest
+from .subgait_test import SubgaitTest
 
 PKG = 'march_shared_classes'
 
@@ -11,3 +12,4 @@ if __name__ == '__main__':
     rosunit.unitrun(PKG, 'limits_test', LimitsTest)
     rosunit.unitrun(PKG, 'setpoint_test', SetpointTest)
     rosunit.unitrun(PKG, 'joint_trajectory_test', JointTrajectoryTest)
+    rosunit.unitrun(PKG, 'joint_trajectory_test', SubgaitTest)
--- march_shared_classes/test/subgait_test.py
+++ march_shared_classes/test/subgait_test.py
@@ -1,9 +1,22 @@
 import unittest
+import rospkg
 
-# from march_shared_classes.gait.subgait import Subgait
-# from march_shared_classes.gait.joint_trajectory import JointTrajectory
+from march_shared_classes.gait.subgait import Subgait
+from march_shared_classes.gait.joint_trajectory import JointTrajectory
+from urdf_parser_py import urdf
+import xacro
 
 
 class SubgaitTest(unittest.TestCase):
-    def test_something(self):
+    def setUp(self):
+        resources_folder = rospkg.RosPack().get_path("march_shared_classes") + "/test/resources"
+        urdf_path = resources_folder + "/march4.urdf"
+        robot = urdf.Robot.from_xml_file(urdf_path)
+        subgait_path = resources_folder + "/walk/left_swing/MV_walk_leftswing_v2.subgait"
+        self.subgait = Subgait.from_file(robot, subgait_path)
+
+    def test_from_file_valid_path(self):
+        self.assertIsNotNone(self.subgait)
+
+    def test_something_2(self):
         self.assertEqual(True, True)
