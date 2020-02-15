036c25c1f436ff7b21bce1e36da434bd80558aec
jorisweeda <joris.weeda@hotmail.com>
2020-02-15 13:46:03 +0100

    Added the transition chain parameter to the walk_state_machine.py to activate the possibility of transitioning of this gait.
    In order to accomplish this variables have to be send between the transitioning states. SMACH uses a userdata container which can be populated during execution of a gait if the variables have been declared on initiation. During execution the gaits with transition possibility will populate these variables.

--- march_state_machine/src/march_state_machine/state_machines/walk_state_machine.py
+++ march_state_machine/src/march_state_machine/state_machines/walk_state_machine.py
@@ -1,10 +1,13 @@
+import smach
+
+from march_state_machine.states.stoppable_state import StoppableState
 from .gait_state_machine import GaitStateMachine
 
 
 class WalkStateMachine(GaitStateMachine):
     """A smach.StateMachine that implements a walking pattern gait."""
 
-    def __init__(self, gait_name):
+    def __init__(self, gait_name, transition_chain=None):
         """Initializes the walking pattern gait.
 
         The gait will be initialized with the following subgaits:
@@ -21,6 +24,13 @@ class WalkStateMachine(GaitStateMachine):
         """
         super(WalkStateMachine, self).__init__(gait_name)
 
+        if transition_chain:
+            self._outcomes = ['succeeded', 'preempted', 'failed', 'transition']
+
+        self._transition_chain = transition_chain
+        self.register_output_keys(['current_gait_name', 'new_gait_name', 'transition_state_name', 'next_state_name'])
+        self.register_input_keys(['start_state'])
+
         self.open()
         self.add_subgait('right_open', succeeded='left_swing')
         self.add_subgait('right_swing', succeeded='left_swing', stopped='left_close')
@@ -28,3 +38,36 @@ class WalkStateMachine(GaitStateMachine):
         self.add_subgait('right_close')
         self.add_subgait('left_close')
         self.close()
+
+    def add_subgait(self, subgait_name, succeeded='succeeded', stopped=None):
+        """Override method to check whether the gait is part of a transition chain"""
+        if self._transition_chain and stopped:
+            smach.StateMachine.add(subgait_name,
+                                   StoppableState(self._gait_name, subgait_name, self._transition_chain),
+                                   transitions={'succeeded': succeeded, 'stopped': stopped, 'aborted': 'failed',
+                                                'transition': 'transition'})
+        else:
+            super(WalkStateMachine, self).add_subgait(subgait_name, succeeded, stopped)
+
+    def _update_once(self):
+        """Set certain output variables in order to construct a possible transition"""
+        self.userdata.current_gait_name = self._gait_name
+        self.userdata.next_state_name = self._current_label
+
+        self.userdata.transition_state_name = None
+        self.userdata.new_gait_name = None
+
+        if self._transition_chain:
+            self.userdata.transition_state_name = self._current_transitions['succeeded']
+
+        return super(GaitStateMachine, self)._update_once()
+
+    def execute(self, parent_ud=smach.UserData()):
+        """Run this function on entry of this state, check if a start gait is passed to the state"""
+        start_state = parent_ud.start_state
+
+        if start_state is not None:
+            self._initial_state_label = start_state
+            parent_ud.next_state_name = None
+
+        return super(WalkStateMachine, self).execute(parent_ud)
--- march_state_machine/src/march_state_machine/states/feedback_action_state.py
+++ march_state_machine/src/march_state_machine/states/feedback_action_state.py
@@ -2,6 +2,8 @@ import smach_ros
 
 
 class FeedbackActionState(smach_ros.SimpleActionState):
-    def __init__(self, action_topic, action, goal, outcomes):
+    def __init__(self, action_topic, action, goal, outcomes, input_keys=None):
+        if input_keys is None:
+            input_keys = []
         super(FeedbackActionState, self).__init__(action_topic, action, goal,
-                                                  outcomes=outcomes)
+                                                  outcomes=outcomes, input_keys=input_keys)
