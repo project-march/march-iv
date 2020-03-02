945e7a2295ce4aab2430aa364e9d88e21de8f4e2
Olav de Haas <o.n.dehaas@student.tudelft.nl>
2020-03-02 16:45:00 +0100

    Make StepStateMachine inherit from GaitStateMachine

--- march_state_machine/src/march_state_machine/state_machines/step_state_machine.py
+++ march_state_machine/src/march_state_machine/state_machines/step_state_machine.py
@@ -1,12 +1,11 @@
-import rospy
 import smach
-from std_msgs.msg import String
 
-from march_state_machine.control_flow import control_flow
 from march_state_machine.states.gait_state import GaitState
 
+from .gait_state_machine import GaitStateMachine
 
-class StepStateMachine(smach.Sequence):
+
+class StepStateMachine(GaitStateMachine):
     """A smach.StateMachine that implements a gait with a sequence of subgaits."""
 
     def __init__(self, gait_name, subgaits=None):
@@ -20,25 +19,13 @@ class StepStateMachine(smach.Sequence):
         :param subgaits: List of subgaits to be performed in the given order.
                          When None, the subgaits will be ['right_open', 'left_close'].
         """
-        super(StepStateMachine, self).__init__(outcomes=['succeeded', 'preempted', 'failed'],
-                                               connector_outcome='succeeded')
-        self._gait_name = gait_name
+        super(StepStateMachine, self).__init__(gait_name)
 
         if subgaits is None:
             subgaits = ['right_open', 'left_close']
 
         self.open()
         for subgait in subgaits:
-            smach.Sequence.add(subgait, GaitState(gait_name, subgait), transitions={'aborted': 'failed'})
+            smach.Sequence.add_auto(subgait, GaitState(gait_name, subgait), connector_outcomes=['succeeded'],
+                                    transitions={'aborted': 'failed'})
         self.close()
-
-        self.register_termination_cb(self._termination_cb)
-        self._gait_publisher = rospy.Publisher('/march/gait/current', String, queue_size=10)
-
-    def execute(self, ud=smach.UserData()):
-        self._gait_publisher.publish(self._gait_name)
-        return super(StepStateMachine, self).execute(ud)
-
-    @staticmethod
-    def _termination_cb(_userdata, _terminal_states, _outcome):
-        control_flow.gait_finished()
